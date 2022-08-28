5a6fb39859dfab796f52787bca41eb498140ed98
limchangyi <limchangyi@gmail.com>
2022-08-28 17:13:50 +0800

    update UI

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      expense tracker
-      <label>Category:</label> 
-      <input type="text" />
-      <label>Amount:</label> 
-      <input type="number" />
-      <label>Date:</label> 
-      <input type="date" />
+      Expense Tracker
+      <div className="ExpenseInfo">
+        <label>Category:</label> 
+        <input type="text" />
+        <label>Amount:</label> 
+        <input type="number" />
+        <label>Date:</label> 
+        <input type="date" />
+        <button>Add Expense</button>
+      </div>
     </div>
   );
 }
