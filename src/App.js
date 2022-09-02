bf6505066b49fa7dd0308647f489f1d5b353cdf0
limchangyi <limchangyi@gmail.com>
2022-09-02 17:07:16 +0800

    update server

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [title, setTitle] = useState("");
+  const [amount, setAmount] = useState(0);
+  const [date, setDate] = useState(Date);
+
+  const displayData = () => {
+    console.log(title + amount + date);
+  }
+
   return (
     <div className="App">
-      Expense Tracker
+      <h1>Expense Tracker</h1>
       <div className="ExpenseInfo">
-        <label>Category:</label> 
-        <input type="text" />
-        <label>Amount:</label> 
-        <input type="number" />
+        <label>Title:</label> 
+        <input type="text" onChange={(event) => {setTitle(event.target.value)}}/>
+        <label>Amount Spent:</label> 
+        <input type="number" onChange={(event) => {setAmount(event.target.value)}}/>
         <label>Date:</label> 
-        <input type="date" />
-        <button>Add Expense</button>
+        <input type="date" onChange={(event) => {setDate(event.target.value)}}/>
+        <button onClick = {displayData}> Add Expense</button>
       </div>
     </div>
   );
